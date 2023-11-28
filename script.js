ee7fd929dc7f2525b24e294b18083c9450afd297
Sophie <hi@hihi.hi>
2023-11-28 16:32:03 +0100

    End of lesson 28.11.2023: Finished exercise 1

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if (currentElement != "e" && currentElement != "E")
     result.push(currentElement)
   }
   return result.join("")
+}
+export function exercise02(args){
+ return args.toUpperCase()
 }
