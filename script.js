757373e0d0c39d3839a00f97224dcc3b6283eb51
Sophie <hi@hihi.hi>
2023-12-07 15:28:00 +0100

    Managed to count 1 "e"! (Exercise 3)

--- script.js
+++ script.js
@@ -5,23 +5,20 @@ export function exercise01(args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement != "e" && currentElement != "E")
-    result.push(currentElement)
+      result.push(currentElement)
   }
   return result.join("")
 }
-export function exercise02(args){
- return args.toUpperCase()
+export function exercise02(args) {
+  return args.toUpperCase()
 }
-export function exercise03(args){
-   const input = args
-   
-   const result = []
-  for (let i = 0; i < input.length; i++){
+export function exercise03(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
     const inputElement = input[i]
     if (inputElement === "e")
-    inputElement.textContent = parseInt(inputElement.textContent) + 1 
-    result.push(inputElement)
+      inputElement.textContent = parseInt(inputElement.textContent) + 1
+    return result.push(inputElement)
   }
-
-return result.join("")  
 }
